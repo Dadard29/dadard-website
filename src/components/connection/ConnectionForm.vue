2c2da25341806db0a56be3e4a0d8523f5ff3ef8e
florian.charpentier <florian.charpentier@epita.fr>
2020-03-02 23:03:56 +0100

    refact form

--- src/components/connection/ConnectionForm.vue
+++ src/components/connection/ConnectionForm.vue
@@ -1,26 +1,46 @@
 <template>
     <div>
-        <h1>Connection</h1>
+        <h3>Connection</h3>
         <form class="form">
             <div class="form-group">
                 <label for="usernameInput">Username</label>
-                <input type="text" class="form-control" id="usernameInput" placeholder="Enter username">
+                <input v-model="username" type="text" class="form-control" id="usernameInput" placeholder="Enter username">
             </div>
             <div class="form-group">
                 <label for="passwordInput">Password</label>
-                <input type="password" class="form-control" id="passwordInput" placeholder="Enter password">
+                <input v-model="password" type="password" class="form-control" id="passwordInput" placeholder="Enter password">
             </div>
             <div class="btn-group" role="group">
-                <button type="submit" class="btn btn-outline-primary">Sign up</button>
-                <button type="submit" class="btn btn-outline-primary">Log in</button>
+                <button v-on:click="signUp" type="button" class="btn btn-outline-primary">Sign up</button>
+                <button v-on:click="logIn" type="button" class="btn btn-outline-primary">Log in</button>
             </div>
+            <span style="color: red">{{errorSignIn}}</span>
         </form>
     </div>
 </template>
 
 <script>
     export default {
-        name: "ConnectionForm"
+        name: "ConnectionForm",
+        data: function () {
+          return {
+              username: null,
+              password: null,
+              errorSignIn: null,
+          }
+        },
+        methods: {
+            logIn: function () {
+                if (this.username == null || this.password == null) {
+                    this.errorSignIn = "bruh"
+                }
+            },
+            signUp: function () {
+                if (this.username == null || this.password == null) {
+                    this.errorSignIn = "bruh"
+                }
+            }
+        }
     }
 </script>
 
